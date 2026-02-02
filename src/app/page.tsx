0ceb6ecae0f059fc6d2c825db9015908bba59f32
Tushar <tushh45@gmail.com>
2026-02-02 22:16:04 +0530

    Spolight Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import HeroSection from "../components/HeroSection";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]" >
-      <h1 className="text-2xl text-center">Tushh Here</h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] pt-20" >
+      
+      <HeroSection />
     </main>
   );
 }
