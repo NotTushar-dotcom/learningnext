6cbb6986a98cb6cde8c9f352f34baa189e6e8ed0
Tushar <tushh45@gmail.com>
2026-02-03 19:03:59 +0530

    fix the moving border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import HeroSection from "../components/HeroSection";
+import { Spotlight } from "@/src/components/ui/Spotlight";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] pt-20" >
-      
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
       <HeroSection />
     </main>
   );
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,10 +7,6 @@ function HeroSection() {
     <div
     className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0"
     >
-        <Spotlight
-        className="-top-40 left-0 md:left-60 md:-top-20"
-        fill="white"
-      />
         <div className="p-4 relative z-10 w-full text-center" >
             <h1
             className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400"
@@ -22,7 +18,7 @@ function HeroSection() {
                 <Link href={"/courses"}>
                     <Button
                     borderRadius="1.75rem"
-                    className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+                    className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800 cursor-pointer"
                     >
                     Explore courses
                     </Button>
